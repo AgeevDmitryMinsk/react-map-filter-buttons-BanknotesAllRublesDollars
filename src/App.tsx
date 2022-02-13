12d96b4dad153bd0ba9bb2c47f333781298d0166
Агеев Дмитрий Александрович <Daageev@minsk.by.dom>
2022-02-13 11:45:31 +0300

    create buttons for filtering selected array all-rubles-dollars

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+type BanknotType = `all` | `rubles` | `dollars`
+
 function App() {
-  return (
+
+    const [money, setMoney] = useState([
+        {banknot: `rubles`, nominal: 100, banknotNumber: `ARX1234567890`  },
+        {banknot: `rubles`, nominal: 100, banknotNumber: `BRX1234567890`  },
+        {banknot: `rubles`, nominal: 100, banknotNumber: `CRX1234567890`  },
+        {banknot: `rubles`, nominal: 100, banknotNumber: `DRX1234567890`  },
+        {banknot: `rubles`, nominal: 100, banknotNumber: `ERX1234567890`  },
+        {banknot: `dollars`, nominal: 100, banknotNumber: `AAX1234567890` },
+        {banknot: `dollars`, nominal: 100, banknotNumber: `BBX1234567890` },
+        {banknot: `dollars`, nominal: 100, banknotNumber: `CCX1234567890` },
+        {banknot: `dollars`, nominal: 100, banknotNumber: `DDX1234567890` },
+    ])
+
+    const [nameBanknoteFiltered, setNameBanknoteFiltered] = useState(`all`)
+    console.log(22, `nameBanknoteFiltered = ${nameBanknoteFiltered}`)
+    let currentBanknote = money
+    if (nameBanknoteFiltered === `rubles`) { currentBanknote = money.filter((x)=>x.banknot===`rubles`)}
+    if (nameBanknoteFiltered === `dollars`) { currentBanknote = money.filter((x)=>x.banknot===`dollars`)}
+
+
+    function onClickHandler(nameBanknote: BanknotType) {
+        setNameBanknoteFiltered(nameBanknote)
+        return console.log(30, `nameBanknote = ${nameBanknote}`, `nameBanknoteFiltered = ${nameBanknoteFiltered}`)
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {currentBanknote.map((x, index)=>{
+            return(
+                <li key={index}>
+                    <span> {x.banknot} </span>
+                    <span> {x.nominal} </span>
+                    <span> {x.banknotNumber} </span>
+                </li>
+            )
+            console.log(x)
+        })}
+        <button onClick={()=>onClickHandler(`all`)}>all</button>
+        <button onClick={()=>onClickHandler(`rubles`)}>rubles</button>
+        <button onClick={()=>onClickHandler(`dollars`)}>dollars</button>
     </div>
   );
 }
